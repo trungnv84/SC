7d7c56483eb688368f940153b6041e4a2daee3aa
trungnv84 <trungnv84@ymail.com>
2013-11-05 08:48:18 +0700

    Core

---
+++ app/core/Controller.php
@@ -0,0 +1,22 @@
+<?php
+defined('ROOT_DIR') || exit;
+
+class Controller
+{
+	public function indexAction()
+	{
+		if (method_exists($this, 'defaultAction'))
+			$this->defaultAction();
+		else App::end('none action -> 404//zzz');
+	}
+
+	protected function assign($key, $value = NULL)
+	{
+		App::assign($key, $value);
+	}
+
+	protected function view($view, $controller = CURRENT_CONTROLLER)
+	{
+		App::view($view, $controller);
+	}
+}
