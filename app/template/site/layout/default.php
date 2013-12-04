6e25000c69a4d7b52f701f32f5ab5688b5ccd5e8
trungnv84 <trungnv84@ymail.com>
2013-12-04 09:16:26 +0700

    Core

--- app/template/site/layout/default.php
+++ app/template/site/layout/default.php
@@ -1,12 +1,11 @@
 <?php defined('ROOT_DIR') || exit;
 
 Tag::unShiftCSS('common.css', 'common', true);
-
 Tag::unShiftCSS('bootstrap/css/bootstrap.min.css', 'bootstrap', true);
-//Tag::addCSS('http://getbootstrap.com/2.3.2/assets/css/bootstrap.css');
-
-Tag::unShiftJS('http://getbootstrap.com/2.3.2/assets/js/bootstrap-alert.js');
 Tag::unShiftJS('bootstrap/js/bootstrap.min.js', 'bootstrap', true);
-Tag::unShiftJS('//code.jquery.com/jquery-1.10.2.min.js', 'jquery', true);
+
+//Tag::addCSS('http://getbootstrap.com/2.3.2/assets/css/bootstrap.css');
+//Tag::unShiftJS('http://getbootstrap.com/2.3.2/assets/js/bootstrap-alert.js');
+//Tag::unShiftJS('//code.jquery.com/jquery-1.10.2.min.js', 'jquery', true);
 
 echo '<div class="main">', $__main_html, '</div>';
