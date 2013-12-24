d5e160bdb7fb36e83a375f6831bc5a0faf4c873e
trungnv84 <trungnv84@ymail.com>
2013-12-24 16:16:34 +0700

    Core

--- public/update/pages/common/navbar.php
+++ public/update/pages/common/navbar.php
@@ -1,4 +1,6 @@
-<nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-default" role="navigation" style="visibility: hidden">
+</nav>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 	<!-- Brand and toggle get grouped for better mobile display -->
 	<div class="navbar-header">
 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -19,7 +21,8 @@
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Management <b class="caret"></b></a>
 				<ul class="dropdown-menu">
 					<li><a href="#">Commit Log</a></li>
-					<li><a href="#">Updated Log</a></li>
+					<li><a href="#">Update Log</a></li>
+					<li><a href="#">Access Log</a></li>
 					<li class="divider"></li>
 					<li><a href="#">List Account</a></li>
 					<li><a href="#">Create Account</a></li>
@@ -37,7 +40,7 @@
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_username = session('user')['username'];?> <b class="caret"></b></a>
 				<ul class="dropdown-menu">
-					<li><a href="?_p=logs&username=<?php echo $_username;?>">My Log</a></li>
+					<li><a href="?_p=logs&username=<?php echo $_username;?>">My Access Log</a></li>
 					<li><a href="?_p=change_password">Change Password</a></li>
 					<li class="divider"></li>
 					<li><a href="?_p=logout">Logout</a></li>
